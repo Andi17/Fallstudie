e05381c7f35b1caaf415212c44073a6eb7eee44e
David <david.noack@outlook.com>
2013-10-11 11:19:16 +0200

    Alpha 0.2

--- FallstudieServer/src/Benutzerverwaltung/Benutzerverwaltung.java
+++ FallstudieServer/src/Benutzerverwaltung/Benutzerverwaltung.java
@@ -1,54 +1,68 @@
 package Benutzerverwaltung;
 
 import java.util.List;
-
+import Optionen.Optionen;
 import Zugriffsschicht.Benutzer;
 import Zugriffsschicht.Zugriffschicht;
 
 public class Benutzerverwaltung {
-	
+
 	private Zugriffschicht dbZugriff;
-	
-	public Benutzerverwaltung (Zugriffschicht dbZugriff) {
+
+	public Benutzerverwaltung(Zugriffschicht dbZugriff) {
 		this.dbZugriff = dbZugriff;
 	}
-	
-	//erstellt einen neuen Benutzer und gibt ein Objekt des entsprechenden Benutzers zurück
-	//vielleicht auch einfach nur boolean als rückgabewert
-	public boolean benutzerErstellen(String benutzername, String passwort, int oeEinheit){
+
+	// Erstellt einen neuen Benutzer und gibt einen boolschen Wert zurÃ¼ck,
+	// welcher darstellt, ob der Benutzer erfolgreich angelegt wurde (true) oder
+	// nicht (false)
+	public boolean benutzerErstellen(String benutzername, String passwort,
+			int oeEinheit) {
+
+		Benutzer neu = dbZugriff.neuerbenutzerErstellen(benutzername, passwort,
+				oeEinheit, Optionen.isInitialbelegungbenutzergesperrt());
+		if (neu == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Holt sich den entsprechenden Benutzer aus der Zugriffsschicht.
+	// gibt null zurÃ¼ck wenn kein Benutzer mit dem Benutzernamen existiert.
+	public Benutzer getBenutzer(String benutzername) {
+		// idBenutzer -> benutzername
+		Benutzer retBenutzer = dbZugriff
+				.getBenutzervonBenutzername(benutzername);
+		return retBenutzer;
+	}
+
+	// RÃ¼ckgabe der verschiedenen Benutzer in einer Liste.
+	public List<Benutzer> getAlleBenutzer() {
+		List<Benutzer> alleBenutzerListe = dbZugriff
+				.AlleBenutzerListeAusgeben();
+		return alleBenutzerListe;
+	}
+
+	// LÃ¶scht den Benuttzer mit der entsprechenden ID.
+	public boolean benutzerLoeschen(String benutzername) {
 		return false;
 	}
 
-	//Holt sich den entsprechenden Benutzer aus der Zugriffsschicht.
-	//gibt null zurück wenn kein Benutzer mit der ID existiert.
-	public Benutzer getBenutzer(String benutzername) {
-		return null;
-	}
-	
-	//Rückgabe der verschiedenen Benutzer in einer Liste.
-	public List<Benutzer> getAlleBenutzer(){
-		return null;
-	}
-	
-	//Löscht den Benuttzer mit der entsprechenden ID.
-	public boolean benutzerLoeschen(String benutzername){
+	// Ã„ndert die OrgaEinheit. Muss aus dem String die entsprechende ID
+	// herrauslesen.
+	public boolean orgaEinheitAendern(String benutzername, String orgaEinheit) {
 		return false;
 	}
-	
-	//Ändert die OrgaEinheit. Muss aus dem String die entsprechende ID herrauslesen.
-	public boolean orgaEinheitAendern(String benutzername, String orgaEinheit){
+
+	// setzt das Passwort des Benutzers mit der entsprechenden benutzerID auf
+	// das neue Passwort
+	public boolean setPasswort(String benutzername, String neuesPasswort) {
 		return false;
 	}
-	
-	//setzt das Passwort des Benutzers mit der entsprechenden benutzerID auf das neue Passwort
-	public boolean setPasswort(String benutzername, String neuesPasswort){
-		return false;
+
+	// Sperrt das Passwort.
+	public void passwortSperren(String benutzername) {
+
 	}
-	
-	//Sperrt das Passwort.
-	public void passwortSperren(String benutzername){
-		
-	}
-	
-	
 }
