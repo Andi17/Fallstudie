5bb04c50e0ea16339f894025c005437aa5402987
Andi <bla@web.de>
2013-10-11 12:23:43 +0200

    Statistik abgeändert

--- FallstudieServer/src/Zugriffsschicht/Statistik.java
+++ FallstudieServer/src/Zugriffsschicht/Statistik.java
@@ -8,96 +8,68 @@ import jdbc.JdbcAccess;
 public class Statistik {
 	private JdbcAccess db;
 	private int idStatistik;
-	private int idOE;
-	private int KW;
-	private int Jahr;
+	private int idOrgaEinheit;
+	private int kalendarWoche;
+	private int jahr;
 	private int idStrichart;
-	private int Strichzahl;
+	private int strichzahl;
 
-	public Statistik(int idStatistik, int idOE, int KW, int Jahr,
-			int idStrichart, int Strichwert, JdbcAccess db) {
-		this.idStatistik = idStatistik;
-		this.idOE = idOE;
-		this.KW = KW;
-		this.Jahr = Jahr;
-		this.idStrichart = idStrichart;
-		this.Strichzahl = Strichwert;
+	// Konstruktor wenn die Statistik schon existiert.
+	public Statistik(ResultSet resultSet, JdbcAccess db) throws SQLException {
+		werteSetzen(resultSet);
 		this.db = db;
 	}
 
-	public Statistik(JdbcAccess db) {
-		this.db = db;
+	// Konstruktor bei Neuanlegen
+	public Statistik(int idOrgaEinheit, int kalendarwoche, int jahr,
+			int idStrichart, int strichanzahl, JdbcAccess db)
+			throws SQLException {
+		db.executeUpdateStatement("INSERT INTO Statistiken (idOrgaEinheit, " +
+				"Kalendarwoche, Jahr, idStrichart, Strichzahl) " +
+				"VALUES ( " + idOrgaEinheit + ", " + kalendarwoche + ", " + jahr +
+				", " + idStrichart + ", " + strichanzahl + ")");
+		ResultSet resultSet = db.executeQueryStatement("SELECT * FROM Statistiken WHERE " +
+				"idOrgaEinheit = " + idOrgaEinheit + ", " +
+				"Kalendarwoche = " + kalendarwoche + ", " +
+				"Jahr = " + jahr + ", " +
+				"idStrichart = " + idStrichart + ", " +
+				"Strichzahl = " + strichanzahl);
+		resultSet.next();
+		werteSetzen(resultSet);
 	}
-
-	public boolean getStatistikfromidStatistik(int idStatistik, JdbcAccess db) {
-		/*
-		 * Statistikdaten werden durch Identifikationsnummer idStatistik
-		 * ermittelt.
-		 */
-		ResultSet resultSet;
-		try {
-			resultSet = db
-					.executeQueryStatement("SELECT * FROM Statistik WHERE idStatistik = '"
-							+ idStatistik + "'");
-			resultSet.next();
-			this.idStatistik = resultSet.getInt("idStatistik");
-			this.idOE = resultSet.getInt("idOE");
-			this.KW = resultSet.getInt("KW");
-			this.Jahr = resultSet.getInt("Jahr");
-			this.idStrichart = resultSet.getInt("idStrichart");
-			this.Strichzahl = resultSet.getInt("Strichwert");
-			resultSet.close();
-		} catch (SQLException e) {
-			return false;
-		}
-		return true;
+	
+	//Methode nur um Kurs zu sparen
+	private void werteSetzen(ResultSet resultSet) throws SQLException{
+		this.idStatistik = resultSet.getInt("idStatistik");
+		this.idOrgaEinheit = resultSet.getInt("idOrgaEinheit");
+		this.kalendarWoche = resultSet.getInt("KalendarWoche");
+		this.jahr = resultSet.getInt("Jahr");
+		this.idStrichart = resultSet.getInt("idStrichart");
+		this.strichzahl = resultSet.getInt("Strichzahl");
 	}
 
 	public int getIDStatistik() {
 		return idStatistik;
 	}
 
-	public int getIDOEt() {
-		return idOE;
+	public int getOrgaEinheit() {
+		return idOrgaEinheit;
 	}
 
-	public int getKW() {
-		return KW;
+	public int getKalendarWoche() {
+		return kalendarWoche;
 	}
 
 	public int getJahr() {
-		return Jahr;
+		return jahr;
 	}
 
-	public int getIDStrichart() {
+	public int getStrichart() {
 		return idStrichart;
 	}
 
-	public int getStrichwert() {
-		return Strichzahl;
+	public int getStrichanzahl() {
+		return strichzahl;
 	}
 
-	public int setIDStatistik() {
-		return this.idStatistik;
-	}
-
-	public int setIDOE() {
-		return this.idOE;
-	}
-
-	public int setKW() {
-		return this.KW;
-	}
-
-	public int setJahr() {
-		return this.Jahr;
-	}
-
-	public int setIDStrichart() {
-		return this.idStrichart;
-	}
-
-	public int setStrichwert() {
-		return this.Strichzahl;
-	}
 }
